0ccacfc12689c914f4bdbfb75ef3407d34cc1199
Diogo <53532032+Di0go@users.noreply.github.com>
2024-01-18 17:00:17 +0000

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://di0go.github.io/game-dev-portfolio/aboutme",
+  base: "https://di0go.github.io/aboutme",
   plugins: [
     react(),
     legacy()
